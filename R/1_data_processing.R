930447b622d641cf2a8112137e0c820e192c9efa
Daniel Noble <daniel.noble@anu.edu.au>
2023-10-21 12:36:48 +1100

    Just cleaning annoting Google sheet names

--- R/1_data_processing.R
+++ R/1_data_processing.R
@@ -13,14 +13,15 @@
 	write.csv(delegates$Email, "output/emails/delegates_emails.csv", row.names = FALSE)
 
 	# Master preprint list 
-	master_list <- read_sheet("https://docs.google.com/spreadsheets/d/1UEAUZWpOm7C1kKoVoYy-u_D6cbHoF--EGxoY0Gl0qHw/edit#gid=836736319", sheet = "Master List of Preprints")
+	master_list <- read_sheet("https://docs.google.com/spreadsheets/d/1UEAUZWpOm7C1kKoVoYy-u_D6cbHoF--EGxoY0Gl0qHw/edit#gid=836736319", sheet = "Master List of Preprints")  %>% clean_names()
 
 	# Check on status
-	status <- master_list  %>% rename("completed" = "Completed_Yes_No_extractor") %>%
+	status <- master_list  %>% rename("completed" = "completed_yes_no_extractor") %>%
 			mutate(completed = str_to_title(completed))  %>% tabyl(completed)
 
 	# raw data
 	data <- read_sheet("https://docs.google.com/spreadsheets/d/1032gLryvtCNJ7eJjKBjrn7txxDRUGql9SoIG18QnFrQ/edit?resourcekey#gid=1062856014")
 
+	data <- data  %>% clean_names()
 
 
