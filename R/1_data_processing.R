7ff730dc972d4253209ff81d8e761b93b09eab1f
Daniel Noble <daniel.noble@anu.edu.au>
2023-10-30 14:36:41 +1100

    Adding code to merge emails

--- R/1_data_processing.R
+++ R/1_data_processing.R
@@ -7,7 +7,7 @@
 
 # Load data from Google Sheets
 	# Delegate list
-	delegates <- read_sheet("https://docs.google.com/spreadsheets/d/1UEAUZWpOm7C1kKoVoYy-u_D6cbHoF--EGxoY0Gl0qHw/edit#gid=836736319", sheet = "Delegate Details")  %>%  clean_names()
+	delegates <- read_sheet("https://docs.google.com/spreadsheets/d/1UEAUZWpOm7C1kKoVoYy-u_D6cbHoF--EGxoY0Gl0qHw/edit#gid=836736319", sheet = "Delegate Details")  %>%  clean_names()  %>% mutate(extractor_full_name = paste0(firstname, " ", lastname))
 
 	# Check on who attended the hackathon
 	delegates  %>% tabyl("attended_hackathon_yes_no")
@@ -34,6 +34,12 @@
 	# Check who hasn't yet started
 		not_done <- contr_extract2  %>% filter(prop != 1)
 	
+	# Grab emails of those not yet done so they can be emailed to be reminded
+		not_done_emails <- left_join(not_done, delegates, by = "extractor_full_name")  %>% select(extractor_full_name, n, yes,no,prop, email) 
+
+	# Write this sheet
+		write.csv(not_done_emails, file = here("output", "emails", "not_done_emails.csv"))
+
 	# raw data
 	data <- read_sheet("https://docs.google.com/spreadsheets/d/1032gLryvtCNJ7eJjKBjrn7txxDRUGql9SoIG18QnFrQ/edit?resourcekey#gid=1062856014") %>% clean_names()
 
