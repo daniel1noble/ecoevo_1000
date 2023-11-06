0dae6ad6e33fd703486444828d4182397894a9a2
Daniel Noble <daniel.noble@anu.edu.au>
2023-11-06 21:04:01 +1100

    Code to export the raw data from Google sheets

--- R/1_data_processing.R
+++ R/1_data_processing.R
@@ -3,7 +3,7 @@
 ###–––––––––––––––––––––––––––––––––––––––––––––––––###
 
 # Load packages
-	pacman::p_load(tidyverse, googlesheets4, janitor)
+	pacman::p_load(tidyverse, googlesheets4, janitor, here)
 
 # Load data from Google Sheets
 	# Delegate list
@@ -19,7 +19,7 @@
 	master_list <- read_sheet("https://docs.google.com/spreadsheets/d/1UEAUZWpOm7C1kKoVoYy-u_D6cbHoF--EGxoY0Gl0qHw/edit#gid=836736319", sheet = "Master List of Preprints")  %>% clean_names()
 
 	# Export master list
-	write.csv(master_list, "output/data/master_list.csv", row.names = FALSE)
+	write.csv(master_list, "data/master_list.csv", row.names = FALSE)
 
 	# Check on status
 	status <- master_list  %>% rename("completed" = "completed_yes_no_extractor") %>%
@@ -40,13 +40,14 @@
 	# Join together delegate list with master list
 		joined <- full_join(delegates, contr_extract2, by = "extractor_full_name", multiple = "all")  %>% arrange(extractor_full_name) %>% data.frame()
 
-
 	# Write this sheet
 		write.csv(not_done_emails, file = here("output", "emails", "not_done_emails.csv"))
 
-	# raw data
-	data <- read_sheet("https://docs.google.com/spreadsheets/d/1032gLryvtCNJ7eJjKBjrn7txxDRUGql9SoIG18QnFrQ/edit?resourcekey#gid=1062856014") %>% clean_names()
+	# raw data, # Problem with the data link article. Some reason it's created it as a list and NULLs are a problem so this needs fixing so that it's a single column not a list within a dataframe
+	data <- read_sheet("https://docs.google.com/spreadsheets/d/1032gLryvtCNJ7eJjKBjrn7txxDRUGql9SoIG18QnFrQ/edit?resourcekey#gid=1062856014") %>% clean_names()  %>% data.frame()   %>% mutate(data_link_article = plyr::ldply(lapply(data_link_article, function(x) ifelse(is.null(x), "NULL", x)))[,1])
 
+	# Write the raw data
+		write.csv(data, file = here("data", "raw_data.csv"))
 
 
 
