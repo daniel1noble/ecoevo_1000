55aa436f4f09da3e43caa91a60a180907dd75602
Daniel Noble <daniel.noble@anu.edu.au>
2023-11-11 08:46:22 +1100

    fix country column.

--- R/1_data_processing.R
+++ R/1_data_processing.R
@@ -70,9 +70,9 @@
 
 # List of to DO
 # - [x] Check that the preprint DOI's are correct and that we have expected number
-# - [] Determine which preprints are missing and collect the data for these
-# - [] Filter out the training paper data (aside from one paper for inclusion in the data)
-# - [] check all pilot data has been removed
+# - [x] Determine which preprints are missing and collect the data for these
+# - [x] Filter out the training paper data (aside from one paper for inclusion in the data)
+# - [x] check all pilot data has been removed
 
 # Have a look at how many preprint DOI's there should be 1216 unique ones
 		data  %>% select(preprint_doi)  %>% distinct()  %>% nrow() #1218....some duplicates clearly
@@ -123,3 +123,64 @@
 # Number of preprints now matches and we have 1216 unique preprints. Lets write this file out before doing some cleaning
 		write.csv(data2, file = here("data", "full_data.csv"))
 
+###-------------------------###
+# Cleaning and fixing mistakes
+# in raw data
+###-------------------------###
+		# Load in the data
+		data2 <- read.csv(here("data", "full_data.csv")) 
+
+# Make sure all data is in the same case structure and strip any white space
+		data2 <- data2  %>% mutate(across(where(is.character), str_to_lower))  %>% mutate(across(where(is.character), str_trim)) 
+
+# Above shoudl solve quite a few issues, now we can explore the data.
+	## Countries - As expected, huge number of problems here to deal with
+		countries <- data2  %>% group_by(submitting_author_country) %>% summarise(n = n())  %>% arrange(desc(n)) 
+
+		# Lets clean this up a bit. remove all instances of "(the)"
+		usa <- c("usa", "united states of america (the)", "us", "unites states of america", "united states", "840") 
+		uk <- c("uk", "united kingdom", "united kingdom of great britain and northern ireland (the)", "united kingdom of great britain and northern ireland", "short name lower case united kingdom of great britain and northern ireland (the)", "the united united kingdom of great britain and northern ireland (the)", "united kingdom of great britain", "united kingdom of great britain and northern ireland (the", "united kingdom of great britain and northern irland (the)", "united kingdom of great britain", "united kingdom (the)", "826") 
+		can <- c("ca", "124")
+		phili <- c("philippines (the)")
+		brazil <- c("brazil iso 3166-2:br", "brasil", "076")
+		netherlands <- c("netherlands (kingdom of the)", "netherlands, kingdom of the", "netherlands (the)", "nl")
+		australia <- c("au", "036")
+		czechia <- c("cz", "203", "the czech republic")
+		finland <- c("finland (the)", "the republic of finland", "246")
+		germany <- c("de")
+		belgium <- c("be", "056")
+		argintina <- c("ar", "032")
+		israel <- c("il", "376")
+		new_zealand <- c("nz", "554")
+		poland <- c("pl", "616")
+		sweden <- c("se", "752")
+		switzerland <- c("ch", "756")
+		ukraine <- c("ua", "804")
+		hungary <- c("hu", "348")
+		korea <- c("kr", "410", "korea (the republic of)")
+		chile <- c("cl", "152", "the republic of chile")
+
+		data2 <- data2  %>% 
+				mutate(submitting_author_country = ifelse(submitting_author_country %in% usa, "united states of america", submitting_author_country),
+				       submitting_author_country = ifelse(submitting_author_country %in% uk, "united kingdom", submitting_author_country),
+				       submitting_author_country = ifelse(submitting_author_country %in% can, "canada", submitting_author_country),
+					   submitting_author_country = ifelse(submitting_author_country %in% phili, "philippines", submitting_author_country),
+					   submitting_author_country = ifelse(submitting_author_country %in% brazil, "brazil", submitting_author_country),
+					   submitting_author_country = ifelse(submitting_author_country %in% netherlands, "netherlands", submitting_author_country),
+					   submitting_author_country = ifelse(submitting_author_country %in% australia, "australia", submitting_author_country),
+					   submitting_author_country = ifelse(submitting_author_country %in% czechia, "czechia", submitting_author_country),
+					   submitting_author_country = ifelse(submitting_author_country %in% finland, "finland", submitting_author_country),
+					   submitting_author_country = ifelse(submitting_author_country %in% germany, "germany", submitting_author_country),
+					   submitting_author_country = ifelse(submitting_author_country %in% belgium, "belgium", submitting_author_country),
+					   submitting_author_country = ifelse(submitting_author_country %in% argintina, "argentina", submitting_author_country),
+					   submitting_author_country = ifelse(submitting_author_country %in% israel, "israel", submitting_author_country),
+					   submitting_author_country = ifelse(submitting_author_country %in% new_zealand, "new zealand", submitting_author_country),
+					   submitting_author_country = ifelse(submitting_author_country %in% poland, "poland", submitting_author_country),
+					   submitting_author_country = ifelse(submitting_author_country %in% sweden, "sweden", submitting_author_country),
+					   submitting_author_country = ifelse(submitting_author_country %in% switzerland, "switzerland", submitting_author_country),
+					   submitting_author_country = ifelse(submitting_author_country %in% ukraine, "ukraine", submitting_author_country),
+					   submitting_author_country = ifelse(submitting_author_country %in% hungary, "hungary", submitting_author_country),
+					   submitting_author_country = ifelse(submitting_author_country %in% korea, "korea", submitting_author_country),
+					   submitting_author_country = ifelse(submitting_author_country %in% chile, "chile", submitting_author_country)) 
+	# Check fixes. Look good aside from missing countries?! (NA) Need to check
+		countries <- data2  %>% group_by(submitting_author_country) %>% summarise(n = n())  %>% arrange(desc(submitting_author_country))  %>% data.frame()
