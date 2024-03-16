a9bf56bfcc3b6896fcaace816a98db9e56186391
Daniel Noble <daniel.noble@anu.edu.au>
2024-03-16 21:59:47 +1100

    Adding some script and then if good moving the relevant scrip to supp.qmd

--- R/3_final_dat_analysis.R
+++ R/3_final_dat_analysis.R
@@ -48,16 +48,34 @@ data2 <- data  %>% left_join(OA_df2, by = c("publisher_doi" = "doi"))
 # We have now cross checked the paper in more detail to see if we missed any published papers. So, we should update the new time between preprint and publication dates, which is a time difference between the preprint and the published paper dates. CRoss checked two times to make sure they were calculated correct. Good!
 
 data2 <- data2  %>% mutate(time_between_preprint_and_pub_days = ymd(publication_date) - ymd(preprint_published_date))
+write.csv(data2, file = here("data", "final_data2.csv"))
 
-
+########-------------------------###
 ### Excluding postprints, how long does it take for a preprint to be published in Ecology and Evolution?
+########-------------------------###
 
 data2  %>% filter(postprint == "preprint")  %>% ggplot(aes(x = time_between_preprint_and_pub_days)) + geom_histogram(binwidth = 30) + labs(title = "Time between preprint and publication in Ecology and Evolution", x = "Days", y = "Frequency") + theme_minimal()
 
-# Why are there weird dates (e.g, Should take at least 3 months to publish a paper, and also, anything below 0 should be a postprint.) Lets assue anything published within 3 months and those with negative dates are postprints.
+# Why are there weird dates (e.g, Should take at least 3 months to publish a paper, and also, anything below 0 should be a postprint.) I suppose it's possible to be published quuite quickly, but a month seems quite weird to me. Lets just take anything below a month out
 
-preprints <- data2  %>% filter(time_between_preprint_and_pub_days >= 90)
-preprints %>% ggplot(aes(x = time_between_preprint_and_pub_days)) + geom_histogram(binwidth = 30) + labs(title = "Time between preprint and publication in Ecology and Evolution", x = "Days", y = "Frequency") + theme_minimal()
+preprints <- data2  %>% filter(time_between_preprint_and_pub_days >= 30)
+preprints %>% ggplot(aes(x = time_between_preprint_and_pub_days)) + geom_histogram(binwidth = 30) + labs(title = "Time between preprint and publication in Ecology and Evolution", x = "Days", y = "Frequency") + theme_classic()
 
 # what is the range
 preprints  %>% summarise(min = min(time_between_preprint_and_pub_days), max = max(time_between_preprint_and_pub_days), mean = mean(time_between_preprint_and_pub_days), median = median(time_between_preprint_and_pub_days), sd = sd(time_between_preprint_and_pub_days))
+
+########-------------------------###
+### What is the OA status of the published preprints?
+########-------------------------###
+
+# Lets first look only at the preprints. Then, we can look at the postprints. It's possible folks are putting up postprints because they are not OA journals and they want to share their work freely.
+
+p1 <- preprints  %>% ggplot(aes(x = is_oa)) + geom_bar() + labs(x = "Open Access", y = "Frequency") + theme_classic()
+
+p2 <- preprints  %>% ggplot(aes(x = oa_status)) + geom_bar() + labs(x = "Open Access Status", y = "Frequency") + theme_classic()
+
+(p1 + p2) + plot_annotation(tag_levels = 'A', tag_suffix=")")
+
+# Summary of the data
+sum_is_oa <- tabyl(preprints$is_oa)
+sum_oa_status <- tabyl(preprints$oa_status)
